3cba68f8333d7e44a0a6b6dac20f86a0968ba341
username <username@cognizant.com>
2026-01-22 16:05:58 +0530

    Fix Netlify 404 routing

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,5 +1,5 @@
 const API_KEY = "RGCwHBwQtHEW8hzaZiwpmLo9Deh3wsUBxGFxmEbU";
-
+//require("dotenv").config();
 export const fetchNews = async (category = "general", page = 1) => {
   const response = await fetch(
     `https://api.thenewsapi.com/v1/news/top?api_token=${API_KEY}&locale=in&categories=${category}&page=${page}&limit=6`
